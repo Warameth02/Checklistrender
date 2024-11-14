ddeb3e2f4bd25dd037b83c10c906767fdbdf9772
Warameth02 <lovevakes2002@gmail.com>
2024-11-14 15:33:42 +0700

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const app = express();
-
 const port = process.env.PORT || 3000;
+const path = require("path");
 
 app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "/views"));
 
 app.get('/', (req, res) => {
     res.render('index')
-})
+});
 
-app.get('/about', (req, res) => {
-    res.render('about')
-})
+app.get('/comparison', (req, res) => {
+    res.render('comparison')
+});
 
 app.listen(port, () => {
     console.log(`Server is runing on port ${port}`)
-})
+});
