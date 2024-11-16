39b715bd7ef14832630369bc53b158b250269c1d
kannasa2002 <2002mantra1998@gmail.com>
2024-11-16 14:27:06 +0700

    first commit

--- api/index.js
+++ api/index.js
@@ -3,14 +3,14 @@ const app = express();
 const path = require("path");
 
 app.set('view engine', 'ejs');
-app.set("views", path.join(__dirname, "/views"));
+app.set("views", path.join(__dirname, "../views"));
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index');
 });
 
 app.get('/comparison', (req, res) => {
-    res.render('comparison')
+    res.render('comparison');
 });
 
 // สร้าง handler สำหรับ Vercel serverless function
